e8376901cd6a15defb94a27e282be9bb205c2c72
Daniel Micay <danielmicay@gmail.com>
2017-05-03 21:54:56 -0400

    add support for verifying page sanitization
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2120,8 +2120,16 @@ static void prep_new_page(struct page *page, unsigned int order, gfp_t gfp_flags
 {
 	post_alloc_hook(page, order, gfp_flags);
 
+	if (IS_ENABLED(CONFIG_PAGE_SANITIZE_VERIFY)) {
+		for (i = 0; i < (1 << order); i++)
+			verify_zero_highpage(page + i);
+	}
+
 	if (!free_pages_prezeroed() && want_init_on_alloc(gfp_flags))
 		kernel_init_free_pages(page, 1 << order);
+
+
+
 
 	if (order && (gfp_flags & __GFP_COMP))
 		prep_compound_page(page, order);
